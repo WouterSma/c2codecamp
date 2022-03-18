19f350d3165ec192b63ca7edf2260b65f3f58f2c
joch3em <70898043+joch3em@users.noreply.github.com>
2022-03-18 09:59:12 +0100

    added an analog clock in index.php

--- index.php
+++ index.php
@@ -10,10 +10,24 @@
 <body>
         <?php require_once("header.php") ?>
    <div class="container">
-        <div id="clock">8:10:45</div>
-        <script src="javascript/clock.js"></script>
-   </div>
+        <div class="clock">
+        <div class="outer-clock-face">
+        <div class="inner-clock-face">
 
+        <div class="hand hour-hand"></div>
+        <div class="hand min-hand"></div>
+        <div class="hand second-hand"></div>
+        </div>
+            <div class="marking marking-one"></div>
+            <div class="marking marking-two"></div>
+            <div class="marking marking-three"></div>
+            <div class="marking marking-four"></div>
+            <script src="javascript/anclock.js"></script>
+        </div>
+        <div id="clock"></div>
+        <script src="javascript/clock.js"></script>
+    </div>
+   </div>
     
    <footer>
    <h3> Neem contact op:</h3>
