1a8d4ef32f2284d2919c29aff0856f303d19d166
Wouter Smaardijk <287574@edu.rocwb.nl>
2022-03-21 11:54:34 +0100

    Create hamers.php

---
+++ tools/hamers.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CodeCamp</title>
+</head>
+<body>
+    <?php require_once("../header.php") ?>
+    <div class="container">
+        <h1>Hamer</h1>
+        <p>Een hamer is een klop- of slagwerktuig. Een hamer bestaat uit een steel, het handvat, met aan het eind de zogenaamde hamerkop. De vorm en grootte en het materiaal van de hamerkop en de lengte van de steel hangen sterk af van het type hamer. Bij hamers met een ijzeren hamerkop staat vaak het aantal grammen in het metaal geslagen. Voor speciale uitvoeringen, zoals voor percussie-instrumenten, wordt ook de term mallet (uit het Engels) gebruikt.</p>
+    </div>
+
+    <footer>
+        <h3> Neem contact op:</h3>
+        <span>email: 12345678910@blabla.hallo</span>
+        <span>Tel. Nummer: 984967486098173289</span>
+   </footer>
+</body>
+</html>
