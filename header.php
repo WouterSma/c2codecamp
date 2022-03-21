f926ac1bc4bcaf0f1607abfb2f21171ba6a1b258
joch3em <70898043+joch3em@users.noreply.github.com>
2022-03-21 11:37:17 +0100

    made the nav bar dropdown menu's

--- header.php
+++ header.php
@@ -1,10 +1,34 @@
 <header>
        <nav>
             <img src="img/logo.png" alt="logo CodeCamp">
-            <h1>CodeCamp</h1>      
+            <h1>CodeCamp</h1>
             <a href="index.php">Home</a>
-            <a href="muziek/muziek.php">Muziek</a>
-            <a href="informatie.php">Informatie</a>
-            <a href="tool.php">tools</a>
+            <div class="dropdown">
+                <a class="dropdown_button" href="muziek/muziek.php">Muziek</a>
+                <div class="dropdown_content">
+                    <a href="#">muziek enzo</a>
+                    <a href="#">muziek enzo</a>
+                    <a href="#">muziek enzo</a>
+                    <a href="#">muziek enzo</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a class="dropdown_button" href="informatie.php">informatie</a>
+                <div class="dropdown_content">
+                    <a href="#">info enzo</a>
+                    <a href="#">info enzo</a>
+                    <a href="#">info enzo</a>
+                    <a href="#">info enzo</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a class="dropdown_button" href="tool.php">tools</a>
+                <div class="dropdown_content">
+                    <a href="#">tools enzo</a>
+                    <a href="#">tools enzo</a>
+                    <a href="#">tools enzo</a>
+                    <a href="#">tools enzo</a>
+                </div>
+            </div>
        </nav>
 </header>
