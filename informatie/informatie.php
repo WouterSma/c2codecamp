5d001958c374b7fa72110eb85d9627bf8bb1f97d
Wouter Smaardijk <287574@edu.rocwb.nl>
2022-03-22 15:45:08 +0100

    #31 Pagina met links gemaakt naar de 3 wesbites

--- informatie/informatie.php
+++ informatie/informatie.php
@@ -9,5 +9,17 @@
 </head>
 <body>
     <?php require_once("../header.php") ?>
+    <div class="container">
+        <h1>Links:</h1>
+        <h3><a href="http://www.imdb.com">IMDB</a></h3>
+        <h3><a href="http://www.videoland.nl">Videoland<a/></h3>
+        <h3><a href="http://www.movies.nl">Movies<a/></h3>
+    </div>
+
+    <footer>
+        <h3> Neem contact op:</h3>
+        <span>email: 12345678910@blabla.hallo</span>
+        <span>Tel. Nummer: 984967486098173289</span>
+    </footer>
 </body>
 </html>
