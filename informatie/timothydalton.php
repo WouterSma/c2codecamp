d4e2e5357c3b2914419417452fc723205894c116
Wouter Smaardijk <287574@edu.rocwb.nl>
2022-03-22 15:54:57 +0100

    #29 created Timothy Dalton page

---
+++ informatie/timothydalton.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>CodeCamp</title>
+</head>
+<body>
+    <?php require_once("../header.php") ?>
+    <div class="container">
+        <div class="information_blue">
+            <h1>Timothy Dalton</h1>
+            <p>Timothy Dalton (Colwyn Bay, 21 maart 1946) is een Brits acteur.</p>
+            <p>Dalton groeide op in Manchester. Nadat hij zijn school had afgemaakt, ging hij naar het National Youth Theatre. Hij studeerde twee jaar lang aan de Royal Academy of Dramatic Art. In 1966 ging hij naar het Birmingham Repertory Theatre, waar hij hoofdrollen speelde. Dalton werd bekend door zijn eerste film, The Lion in Winter, waarin hij de koning van Frankrijk speelde. Omdat Pierce Brosnan door contractuele verplichtingen niet in aanmerking kwam als de opvolger van Roger Moore, werd Dalton gevraagd om de hoofdrol te spelen in de James Bondfilms. Hij zette zich er persoonlijk voor in om de Bondfilms in de geest van de boeken van Ian Fleming te maken. Hij las de boeken en zette een Bond neer die sterk overeenkwam met de persoon zoals Fleming die bedacht had. Hij speelde in de The Living Daylights (1987, met Bondmeisje Maryam d'Abo) en in Licence to Kill (1989, Bondmeisjes Talisa Soto en Carey Lowell). Daarna stopte Dalton met Bond, ging hij acteren in de televisieserie Scarlett en had hij een bijrol in de film Hot Fuzz. Hij speelde verder Julius Caesar in de televisiefilm Cleopatra uit 1999.
+
+Hij speelde ook in veel Britse kostuumdrama's, waaronder Wuthering Heights (1970) en Jane Eyre (1983).</p>
+
+
+        </div>
+    </div>
+
+    <footer>
+        <h3> Neem contact op:</h3>
+        <span>email: 12345678910@blabla.hallo</span>
+        <span>Tel. Nummer: 984967486098173289</span>
+    </footer>
+</body>
+</html>
