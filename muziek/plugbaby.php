ce42e60c2989d2a17d4f6c4751b874d46d3d5604
Damian-vervaart <71429628+Damian-vervaart@users.noreply.github.com>
2022-03-23 10:36:21 +0100

    ticket #19 done
    added the html and styling for ticket #19

--- muziek/plugbaby.php
+++ muziek/plugbaby.php
@@ -7,7 +7,40 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
+<body class="redpage">
+
+
+    <?php require_once "../header.php"; ?>
+
+        <div class="lyric">   
+            <p>
+                I've exposed your lies, baby<br>
+                The underneath no big surprise<br>
+                Now it's time for changing<br>
+                And cleansing everything<br>
+                To forget your love<br>
+                My plug in baby<br>
+                Crucifies my enemies<br>
+                When I'm tired of giving<br>
+                My plug in baby<br>
+                In unbroken virgin realities<br>
+                Is tired of living<br>
+                Don't confuse<br>
+                Baby you're gonna lose<br>
+                Your own game<br>
+                Change me<br>
+                And replace the envying<br>
+                To forget your love<br>
+                My plug in baby<br>
+                Crucifies my enemies<br>
+                When I'm tired of giving<br>
+                My plug in baby<br>
+                In unbroken virgin realities<br>
+                And tired of living<br>
+                And I've seen your loving<br>
+                Mine is gone<br>
+            </p>
+        </div> 
     
 </body>
 </html>
