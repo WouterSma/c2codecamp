5664e81a1d02deff1f79345656b5d6d379a523d2
KasperSNL <75014907+KasperSNL@users.noreply.github.com>
2022-03-23 12:17:07 +0100

    set up Killer Queen page

--- muziek/killer.php
+++ muziek/killer.php
@@ -7,7 +7,60 @@
     <link rel="stylesheet" href="css/style.css">
     <title>C2CodeCamp</title>
 </head>
-<body>
+<body class="queen">
     <?php require_once "../header.php"?>
+
+    <div class="lyric">
+    <p> She keeps her Moet et Chandon<br>
+        In her pretty cabinet<br>
+        "Let them eat cake", she says<br>
+        Just like Marie Antoinette<br>
+        A built-in remedy<br>
+        For Khrushchev and Kennedy<br>
+        At anytime an invitation<br>
+        You can't decline<br>
+        Caviar and cigarettes<br>
+        Well versed in etiquette<br>
+        Extraordinarily nice<br>
+        She's a Killer Queen<br>
+        Gunpowder, gelatine<br>
+        Dynamite with a laser beam<br>
+        Guaranteed to blow your mind<br>
+        Anytime<br>
+        Recommended at the price<br>
+        Insatiable an appetite<br>
+        Wanna try?<br>
+        To avoid complications<br>
+        She never kept the same address<br>
+        In conversation<br>
+        She spoke just like a baroness<br>
+        Met a man from China<br>
+        Went down to Geisha Minor<br>
+        Then again incidentally<br>
+        If you're that way inclined<br>
+        Perfume came naturally from Paris (naturally)<br>
+        For cars she couldn't care less<br>
+        Fastidious and precise<br>
+        She's a Killer Queen<br>
+        Gunpowder, gelatine<br>
+        Dynamite with a laser beam<br>
+        Guaranteed to blow your mind<br>
+        Anytime<br>
+        Drop of a hat she's as willing as<br>
+        Playful as a pussy cat<br>
+        Then momentarily out of action<br>
+        Temporarily out of gas<br>
+        To absolutely drive you wild, wild<br>
+        She's all out to get you<br>
+        She's a Killer Queen<br>
+        Gunpowder, gelatine<br>
+        Dynamite with a laser beam<br>
+        Guaranteed to blow your mind<br>
+        Anytime<br>
+        Recommended at the price<br>
+        Insatiable an appetite<br>
+        Wanna try?<br>
+        You wanna try<br>
+    </p>
 </body>
 </html>
