028667504731a99949ab337309ed98d4bed4bdf1
Wouter Smaardijk <287574@edu.rocwb.nl>
2022-04-05 15:52:07 +0200

    #53 aanmaakpagina voor muziek gemaakt

--- muziek/Muziek.php
+++ muziek/Muziek.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<?php session_start(); ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,7 +7,28 @@
     <link rel="stylesheet" href="css/style.css">
     <title>CodeCamp</title>
 </head>
+<?php 
+    require_once '../backend/conn.php';
+    $query = "SELECT * FROM music";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $songs = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
 <body>
     <?php require_once("../header.php") ?>
+    <div class="books">
+    <a href="create.php" class="dropdown_button">Post a song</a>
+        <?php foreach($songs as $song): ?>
+        <div class="book">
+            <h3><?php echo $song['title'] ?></h3>
+            <h4>By: <?php echo $song['artist'] ?></h4>
+            <p><?php echo $song['songtext']?></p>
+            <a href="edit.php?id=<?php echo $song['id'] ?>">edit</a>
+        </div>
+        <?php endforeach; ?>
+    </div>
+    <?php require_once"../footer.php"?>
+
+    
 </body>
 </html>
