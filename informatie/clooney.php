cac1d4ec0770689a3f5b86dcfb7f3826ac3e367e
KasperSNL <75014907+KasperSNL@users.noreply.github.com>
2022-04-05 14:50:54 +0200

    set up clooney.php

---
+++ informatie/clooney.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>CodeCamp</title>
+</head>
+<body class="clooney">
+    <?php require_once("../header.php") ?>
+
+    <div class="container">
+
+    <footer>
+        <?php require_once("../footer.php") ?>
+    </footer>
+</body>
+</html>
