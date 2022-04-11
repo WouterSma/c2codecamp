cd8b6234f6ef94b21a4150874390cc14816eaa15
joch3em <70898043+joch3em@users.noreply.github.com>
2022-04-11 13:12:46 +0200

    made the price changable by sales manager

---
+++ books/editPrice.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>CodeCamp</title>
+</head>
+
+<body>
+    <?php require_once "../header.php"; ?>
+    <?php
+        if(empty($_SESSION['user_id'])){
+            die("You have too be a sales manager too edit the price of a book!");
+        }
+        elseif(!$_SESSION['role'] == "sales_manager"){
+            die("You have too be a sales manager too edit the price of a book!");
+        }
+    ?>
+    <?php 
+    $id = $_GET['id'];
+    require_once "../backend/conn.php";
+    $query = "SELECT * FROM books WHERE id = :id";
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":id"=>$id
+    ]);
+    $book = $statement->fetch(PDO::FETCH_ASSOC);
+    ?>
+    <div class="container">
+        <form action="../backend/bookController.php" method="POST">
+            <input type="hidden" name="action" value="edit_price">
+            <input type="hidden" name="id" value="<?php echo $book['id'] ?>">
+            <div class="form-group">
+                <label for="price">Price: </label>
+                <input type="price" name="price" id="price" class="form-input">
+            </div>
+            <input type="submit" value="Post price">
+        </form>
+    </div>
+</body>
